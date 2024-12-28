a5a6b02b5492332b9993409a9c764ba26240076d
furkanerenakinci <erenakincii0@gmail.com>
2024-12-28 22:15:51 +0300

    Initial Commit

---
+++ CatchTheKenny/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CatchTheKenny
+//
+//  Created by Furkan Eren Akıncı on 28.12.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
